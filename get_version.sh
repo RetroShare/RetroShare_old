0ba262a8ca5bdacb4415e7864c89d489ce45474f
Chozabu <chozabu@gmail.com>
2015-07-10 13:48:28 +0100

    clean indentation back to tabs!

--- get_version.sh
+++ get_version.sh
@@ -12,24 +12,24 @@ if ( git log -n 1 &> /dev/null); then
 	
 	#try to try and pull SVN revision from last commit
 	cleverversion="$(git log -n 1 | awk '/svn/ {print $2}' | head -1 | sed 's/.*@//')"
-        if [ -z $cleverversion ]; then
-            #"failed to get SVN version from most recent git"
-            cleverversion="$(git log -n 10 | awk '/svn/ {print $2}' | head -1 | sed 's/.*@//')"
-        fi
-        
+		if [ -z $cleverversion ]; then
+			#"failed to get SVN version from most recent git"
+			cleverversion="$(git log -n 10 | awk '/svn/ {print $2}' | head -1 | sed 's/.*@//')"
+		fi
+		
 	#try to try and pull SVN revision from last 10 commits
-        if [ $cleverversion ]; then
-            #"got clever version"
-            version=$cleverversion
-        fi
+		if [ $cleverversion ]; then
+			#"got clever version"
+			version=$cleverversion
+		fi
 	
 	#if not got a version using smart method, use easy method
-        if [ $version == 0 ]; then
-            #retrieve git information - just get number of commits
-            #note - this will not match svn commits
-            #note - with caves repo - start of history is cutoff? so it is unusually small number
-            version=$(git rev-list HEAD | wc -l)
-        fi
+		if [ $version == 0 ]; then
+			#retrieve git information - just get number of commits
+			#note - this will not match svn commits
+			#note - with caves repo - start of history is cutoff? so it is unusually small number
+			version=$(git rev-list HEAD | wc -l)
+		fi
 	
 	vcs="git"
 	githash=$(git log --format="%H" -n 1)
@@ -38,20 +38,20 @@ elif ( svn info &> /dev/null); then
 	version=$(svn info | awk '/^Revision:/ {print $NF}')
 	vcs="svn"
 else
-    version=0001
-    vcs="error"
+	version=0001
+	vcs="error"
 fi
 
 #echo "version is $version from $vcs"
 if [ "$1" == "vcs" ]; then
-    echo $vcs
-    exit 0
+	echo $vcs
+	exit 0
 fi
 
 
 if [ "$1" == "githash" ]; then
-    echo $githash
-    exit 0
+	echo $githash
+	exit 0
 fi
 
 echo $version
--- retroshare-gui/src/version_detail.sh
+++ retroshare-gui/src/version_detail.sh
@@ -11,16 +11,16 @@ set +e
 if (ls &> /dev/null); then
 	echo "Retroshare Gui version : " > gui/help/version.html
 	
-        version=$(../../get_version.sh)
+		version=$(../../get_version.sh)
 
-        vcs=$(../../get_version.sh vcs)
-        githash=$(../../get_version.sh githash)
-        
-        echo "VCS: $vcs" >> gui/help/version.html
-        if [ "$vcs" == "git" ]; then
-            echo "Git Hash : $githash" >> gui/help/version.html
-        fi
-        echo "Revision Number: $version" >> gui/help/version.html
+		vcs=$(../../get_version.sh vcs)
+		githash=$(../../get_version.sh githash)
+		
+		echo "VCS: $vcs" >> gui/help/version.html
+		if [ "$vcs" == "git" ]; then
+			echo "Git Hash : $githash" >> gui/help/version.html
+		fi
+		echo "Revision Number: $version" >> gui/help/version.html
 	date >> gui/help/version.html
 	echo "" >> gui/help/version.html
 	echo "" >> gui/help/version.html
